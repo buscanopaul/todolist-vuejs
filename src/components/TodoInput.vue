ea5564bbc0546e4653d3db81f2f2451b7b367914
buscanopaul <buscano.paul@gmail.com>
2021-10-25 21:03:51 +0800

    Adds uuid for unique id and test add function

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,14 +1,28 @@
 <template>
     <div class="row">
-        <input class="col form-control mx-2" type="text" />
-        <button class="btn btn-primary">Add </button>
+        <input v-model="todoText" class="col form-control mx-2" type="text" />
+        <button @click="addTodo(todoText)" class="btn btn-primary">Add </button>
     </div>
 </template>
 
 <script>
 
+import { mapActions } from 'vuex';
+
 export default {
- 
+    data() {
+        return {
+            todoText: ""
+        };
+    },
+    methods: {
+        ...mapActions(['addTodo']),
+        addTodo() {
+            this.addTodo({
+                title: this.todoText
+            });
+        }
+    }
 };
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,36 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    todos: [
+    {
+      id: 1,
+      title: "One"
+    },
+    {
+      id: 2,
+      title: "Two"
+    },
+    {
+      id: 3,
+      title: "Three"
+    }
+  ],
   },
   mutations: {
+    add_todo(state, todo) {
+      state.todos.push(todo);
+      console.log(todo);
+    },
   },
   actions: {
+    addTodo({commit}, todo) {
+      commit('add_todo', todo);
+    }
   },
   modules: {
-  }
+  },
+  getters: {
+    allTodos: (state)=>state.todos,
+  },
 })
+ 
