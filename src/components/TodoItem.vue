2d67e664c80849d4405b51e7488eceaefad3ba62
buscanopaul <buscano.paul@gmail.com>
2021-10-25 21:32:52 +0800

    Adds delete method

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,15 +1,19 @@
 <template>
       <th scope="row">1</th>
       <td>{{todo.title}}</td>
-      <td><button class="btn btn-warning">Edit</button> <button class="btn btn-danger">Delete</button></td>
+      <td><button class="btn btn-warning">Edit</button> <button @click="deleteTodo(todo.id)" class="btn btn-danger">Delete</button></td>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 
 export default {
     props: {
         todo: {
         }
+    },
+    methods: {
+        ...mapActions(['deleteTodo'])
     }
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -20,12 +20,17 @@ export default createStore({
   mutations: {
     add_todo(state, todo) {
       state.todos.push(todo);
-      console.log(todo);
+    },
+    delete_todo(state,id) {
+      state.todos = state.todos.filter((todo) => todo.id != id);
     },
   },
   actions: {
     addTodo({commit}, todo) {
       commit('add_todo', todo);
+    },
+    deleteTodo({commit}, id) {
+      commit('delete_todo', id);
     }
   },
   modules: {
